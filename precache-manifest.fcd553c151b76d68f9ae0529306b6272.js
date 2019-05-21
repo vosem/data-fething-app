05638b135dd610b83068b7cf4db07165461fff6d
vosem <anna.v.popovska@gmail.com>
2019-05-21 13:53:10 +0300

    Updates

---
+++ precache-manifest.fcd553c151b76d68f9ae0529306b6272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6db13aa162d9024500a1e43ba4a2de",
+    "url": "/data-fetching-app/index.html"
+  },
+  {
+    "revision": "dd35a4578845b6910752",
+    "url": "/data-fetching-app/static/css/main.20fdd910.chunk.css"
+  },
+  {
+    "revision": "89b9983be65c3b3bd655",
+    "url": "/data-fetching-app/static/js/2.e40262dd.chunk.js"
+  },
+  {
+    "revision": "dd35a4578845b6910752",
+    "url": "/data-fetching-app/static/js/main.eb641dfb.chunk.js"
+  },
+  {
+    "revision": "a18e750713803afba099",
+    "url": "/data-fetching-app/static/js/runtime~main.2df40edd.js"
+  },
+  {
+    "revision": "eac23845ae3648d06a21e199f75c5432",
+    "url": "/data-fetching-app/static/media/no_poster.eac23845.jpg"
+  }
+]);
